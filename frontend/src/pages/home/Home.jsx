ec055773a30b65debb0432971d8169af8f67ecf2
Shriyash Thorat <Shriyash.Thorat@ril.com>
2024-11-15 12:41:30 +0530

    initial commit

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Banner from "./Banner";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      
+    </>
+  );
+};
+
+export default Home;
