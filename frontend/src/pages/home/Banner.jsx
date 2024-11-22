90de0922c1b218b0186528f5588d3855554a63fe
Shriyash Thorat <Shriyash.Thorat@ril.com>
2024-11-22 17:12:35 +0530

    created home page

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -3,8 +3,11 @@ import bannerImg from "../../assets/banner.png";
 
 const Banner = () => {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12">
+      <div className="md:w-1/2 w-full flex items-center md:justify-end">
+        <img src={bannerImg} />
+      </div>
+      <div className="md:w-1/2 w-full">
         <h1 className="md:text-5xl text-2xl font-medium mb-7">
           New Releases This Week
         </h1>
@@ -15,9 +18,6 @@ const Banner = () => {
           everyone
         </p>
         <button className="btn-primary">Subscribe</button>
-      </div>
-      <div>
-        <img src={bannerImg} />
       </div>
     </div>
   );
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Banner from "./Banner";
+import Topseller from "./Topseller";
+import Recommened from "./Recommened";
+import News from "./News";
 
 const Home = () => {
   return (
     <>
       <Banner />
-      
+      <Topseller />
+      <Recommened />
+      <News />
     </>
   );
 };
